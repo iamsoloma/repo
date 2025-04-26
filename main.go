d550508a57ed70260810876b5405d6ab2d8ea6ee
soloma <EgorSolomahin1@yandex.ru>
2025-04-26 16:37:35 +0500

    cgi server

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/cgi"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	reposRoot := os.Getenv("GIT_PROJECT_ROOT")
+	if reposRoot == "" {
+		log.Fatal("GIT_PROJECT_ROOT env variable is not set")
+	}
+
+	gitPath, err := exec.LookPath("git")
+	if err != nil {
+		log.Fatalf("cannot find git: %v", err)
+	}
+	log.Printf("using a git at: %q", gitPath)
+
+	gitHandler := &cgi.Handler{
+		Path: gitPath,
+		Args: []string{"http-backend"},
+		Env: []string{
+			fmt.Sprintf("GIT_PROJECT_ROOT=%s", reposRoot),
+			"GIT_HTTP_EXPORT_ALL=true",
+		},
+	}
+
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", gitHandler))
+
+}
