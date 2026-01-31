82630a96554b429db4fa1727f0793ddaceaef37a
soloma <EgorSolomahin1@yandex.ru>
2026-02-01 00:59:36 +0500

    main.go <- a working git server

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module repo
 
 go 1.24.2
 
-require github.com/go-git/go-git/v5 v5.16.0
+require (
+	github.com/go-git/go-git/v5 v5.16.0
+	github.com/sosedoff/gitkit v0.4.0
+)
 
 require (
 	dario.cat/mergo v1.0.0 // indirect
@@ -13,6 +16,7 @@ require (
 	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
 	github.com/go-git/go-billy/v5 v5.6.2 // indirect
+	github.com/gofrs/uuid v4.0.0+incompatible // indirect
 	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/kevinburke/ssh_config v1.2.0 // indirect
--- main.go
+++ main.go
@@ -1,12 +1,43 @@
 package main
 
-import "repo/api"
+import (
+	"log"
+	"net/http"
+
+	"github.com/sosedoff/gitkit"
+)
 
 func main() {
-	config := api.Config{
-		ListenAddr:  "0.0.0.0:3000",
-		ReposFolder: "./repos",
+	// Configure git hooks
+	hooks := &gitkit.HookScripts{
+		PreReceive: `echo "Hello World!"`,
+		PostReceive: `echo "Hello World!" > file.txt`,
 	}
-	server := api.NewServer(config)
-	server.Start()
+
+	// Configure git service
+	service := gitkit.New(gitkit.Config{
+		Dir:        "./repos",
+		AutoCreate: true,
+		AutoHooks:  true,
+		Auth:       true,
+		Hooks:      hooks,
+	})
+
+	service.AuthFunc = func(c gitkit.Credential, r *gitkit.Request) (bool, error) {
+		log.Println("Auth: ", c.Username, c.Password, r.RepoName)
+		return c.Username == "soloma", nil
+	}
+
+	// Configure git server. Will create git repos path if it does not exist.
+	// If hooks are set, it will also update all repos with new version of hook scripts.
+	if err := service.Setup(); err != nil {
+		log.Fatal(err)
+	}
+
+	http.Handle("/", service)
+
+	// Start HTTP server
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+		log.Fatal(err)
+	}
 }
